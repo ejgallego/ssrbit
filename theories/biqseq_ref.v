01c258259d5b93bd951ae7d9d7e670989027ebc0
Emilio Jesus Gallego Arias <e+git@x80.org>
2016-06-03 17:20:55 +0200

    Proof of direct refinement from ffun to seq.
    It has of course to be refactored but it should serve as a base.

--- theories/biqseq_ref.v
+++ theories/biqseq_ref.v
@@ -21,6 +21,10 @@ Require Import bigop ssralg ssrnum fingroup perm finalg zmodp ssrint.
 From CoqEAL
 Require Import hrel refinements pos.
 
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
 Require Import bitseq.
 
 (* Refinements by P-E Dagand *)
@@ -29,25 +33,32 @@ Section BitFFun.
 Variable k : nat.
 
 Definition BitSeq := {ffun 'I_k -> bool}.
-Implicit Type (b : k.-tuple bool) (f : BitSeq).
+Implicit Type (b : bitseq) (f : BitSeq).
 
-Definition orB b1 b2          := [tuple of liftz false orb b1 b2].
+Definition orB b1 b2          := liftz false orb b1 b2.
 Definition orF f1 f2 : BitSeq := [ffun j => f1 j || f2 j].
 
-Definition R_bitseq b f : Prop := f =1 getb b.
+Definition R_bitseq b f : Prop := b = fgraph f.
 Notation "b ≈ f" := (R_bitseq b f) (at level 42).
 (* Even if like this more *)
 (* Definition R_bitseq b f : bool := [forall j, f j == getb b j]. *)
 
-Lemma getb_liftz (j : 'I_k) d op b1 b2 :
-  getb (liftz d op b1 b2) j = op (nth d b1 j) (nth d b2 j).
-Proof.
-rewrite /getb /liftz (nth_map (d, d)) ?nth_zipd //.
-by rewrite size_zipd !size_tuple maxnn.
-Qed.
+Lemma size_ref b f : b ≈ f -> size b = k.
+Proof. by move ->; rewrite size_tuple card_ord. Qed.
 
-Lemma R_or b1 f1 (hR1 : b1 ≈ f1) b2 f2 (hR2 : b2 ≈ f2) : [tuple of orB b1 b2] ≈ orF f1 f2.
-Proof. by move=> x; rewrite ffunE hR1 hR2 getb_liftz. Qed.
+Lemma R_or b1 f1 (hR1 : b1 ≈ f1) b2 f2 (hR2 : b2 ≈ f2) : orB b1 b2 ≈ orF f1 f2.
+Proof.
+have sz1 := size_ref hR1.
+have sz2 := size_ref hR2.
+have szO : size (orB b1 b2) = k by rewrite size_liftz sz1 sz2 maxnn.
+apply: (@eq_from_nth _ false); first by rewrite szO size_tuple card_ord.
+move=> i i_lt; rewrite /liftz (nth_map (false,false)).
+  have lt : i < #|'I_k| by rewrite card_ord -szO.
+  have U x : ((fgraph x)`_i)%B = tnth (fgraph x) (Ordinal lt).
+    by rewrite (tnth_nth false).
+  by rewrite nth_zipd hR1 hR2 !U /orF !tnth_fgraph ffunE.
+by rewrite size_zipd sz1 sz2 maxnn -szO.
+Qed.
 
 Global Instance or_refineP : refines (R_bitseq ==> R_bitseq ==> R_bitseq)%rel orB orF.
 Proof. by rewrite refinesE; exact: R_or. Qed.
