cec146a2ea31695321713d9b2bc4df42d895fd74
Emilio Jesus Gallego Arias <e+git@x80.org>
2016-07-21 00:30:21 +0200

    [cardinal] Progress on new proof.
    The main idea is here, however we need some more tweaks.
    
    Tried to factor out a bit the table generation.

--- theories/bitocaml.v
+++ theories/bitocaml.v
@@ -14,7 +14,7 @@
 From mathcomp
 Require Import ssreflect ssrfun ssrbool eqtype ssrnat seq div.
 From mathcomp
-Require Import choice fintype finset tuple path bigop.
+Require Import choice fintype finfun finset tuple path bigop.
 
 (******************************************************************************)
 (* Convenience Theory for extraction to ocaml integers                        *)
@@ -38,46 +38,95 @@ Unset Printing Implicit Defensive.
 Open Scope bits_scope.
 
 (* Optimized cardinality *)
+Definition tobit k o := [bits of bitn k o].
+
 Section Card.
 
-Implicit Types (n k : nat).
+Section CardDef.
+
+Variable (n k : nat).
+
+(* Cardinality table *)
+Definition ctable  := seq 'B_n.
+Definition ctableP (t : ctable) :=
+  forall (bk : 'B_k), nth '0 t (nats bk) = tobit n (count id bk).
+
+(** [pop_table] returns a k-list of words of size [n]
+  * counting the numbers of bits.
+  *)
+Definition pop_table : seq 'B_n :=
+  mkseq (fun i => tobit n (count id (bitn k i))) (2^k).
+
+Lemma pop_table_size : size pop_table = 2^k.
+Proof. by rewrite size_mkseq. Qed.
+
+Lemma pop_tableP : ctableP pop_table.
+Proof.
+move=> bk; rewrite nth_mkseq -?{1}(size_tuple bk) ?natsK //.
+by rewrite -{2}(size_tuple bk) nats_ltn.
+Qed.
+
+(* An alternative way is to define pop_table as a map: *)
+Definition pop_table_fun {n} k : {ffun 'B_k -> 'B_n} :=
+  [ffun bk => [bits of bitn n (count id (val bk))]].
+
+Lemma pop_table_funP (bk : 'B_k) :
+  pop_table_fun k bk = tobit n (count id bk).
+Proof. by rewrite ffunE. Qed.
+
+(** [pop_elem bs i] return the cardinality of the [i]-th part of
+  * of [bs].
+  *)
+Definition pop_elem (bs: 'B_n) i : 'B_n :=
+  let x := andB (shrB bs (i * k))
+                (decB (shlB [bits of bitn n 1] k))
+  in nth '0 pop_table (nats x).
+
+(* Don't we want to shift here maybe ? *)
+Definition card_rec (bs: 'B_n) := fix cr i : 'B_n :=
+    match i with
+    | 0     => '0
+    | i'.+1 => addB (pop_elem bs i') (cr i')
+    end.
+
+(** [cardinal k bs] returns the number of bits set in [bs] to one using a
+  * table of size [k]
+  *)
+Definition cardinal (bs: 'B_n) : 'B_n := card_rec bs (n %/ k).
+
+End CardDef.
+
+Eval compute in (map val (@pop_table 4 2)).
+Eval compute in (val (@pop_elem 3 1 [tuple true; false; true] 0)).
+Eval compute in (val (cardinal 2 [tuple true; true])).
+
+Section CardTheory.
+
+Lemma card_rec_cons n k i (bv : 'B_n) :
+  card_rec k bv i.+1 =
+  addB (pop_elem k bv i) (card_rec k bv i).
+Proof. by []. Qed.
 
 (* ss is a partition of S then *)
 (*  "cardb S = \sum(s <- ss) cardb s" *)
+Lemma cardinalE n k (bv : 'B_n) :
+  nats (cardinal k bv) =
+  \sum_(pbv <- reshape (nseq (n %/ k) k) bv) (nats pbv).
+Proof.
+rewrite /cardinal.
+elim: (n %/ k) {1 2 3 6}n bv => [|nk ihnk] nv bv.
+  by rewrite nats_zero big_nil.
+rewrite card_rec_cons [reshape _ _]/= big_cons -ihnk.
+Admitted.
 
-Definition bit_tmp k o := [tuple of bitn k o]. (* XXX: Emilio? *)
-
-(* XXX: Factor out k of the recursion *)
-Definition pop_table {n} k : seq 'B_n :=
-  mkseq (fun i => bit_tmp n (count id (bitn k i))) (2^k).
-
-Eval compute in (map val (@pop_table 4 2)).
-
-Definition pop_elem n k (bs: 'B_n) i : 'B_n :=
-  let x :=
-      andB (shrB bs (i * k))
-           (decB (shlB [bits of bitn n 1] k))
-  in nth '0 (pop_table k) (nats x).
-
-Eval compute in (val (@pop_elem 3 1 [tuple true; false; true] 0)).
-
-Fixpoint popAux n k (bs: 'B_n) i : 'B_n :=
-  match i with
-  | 0     => '0
-  | i'.+1 => addB (pop_elem k bs i') (popAux k bs i')
-  end.
-
-Definition cardinal {n} (k: nat)(bs: 'B_n): 'B_n :=
-  popAux k bs (n %/ k).
-
-Eval compute in (map val (@pop_table 4 2)).
-Eval compute in (val (cardinal 1 [tuple true; false])).
+(* Lemma cardinalE : \sum_(l reshape  *)
 
 Lemma cardinalP k (s : 'B_k) i (div_i: i %| k) (ltz_i: i > 0) :
   nats (cardinal i s) = count id s.
 Proof.
 Admitted.
 
+End CardTheory.
 End Card.
 
 Section BitMin.
