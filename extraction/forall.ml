948f5da6eb3b1d450c818c57cea690a67408a852
Emilio Jesus Gallego Arias <e+git@x80.org>
2016-07-23 15:58:31 +0200

    [extraction] Fix time report.
    Still not meaningful for tests involving forall2.

--- extraction/forall.ml
+++ extraction/forall.ml
@@ -4,7 +4,7 @@ exception TestFailure of int
 
 let forall_int wordsize k =
   let upb = (1 lsl wordsize) - 1       in
-  let upc = upb / 100                  in
+  let upc = upb / 20                   in
   let rtm = ref (Unix.gettimeofday ()) in
   printf "Starting, upb: %d@\n%!" upb;
 
@@ -15,12 +15,17 @@ let forall_int wordsize k =
           printf "%3d %% done@\n%!" (i*100/upb + 1);
           let rtn = Unix.gettimeofday () in
           if i != 0 then
-            printf "time remaining %d secs@\n%!" ((int_of_float (rtn -. !rtm)) * (upb-i) / upc);
+            let it_time = int_of_float ((rtn -. !rtm) *. 1000000.0) in
+            let msecs   = it_time * (upb-i) / upc / 1000            in
+            let secs    = msecs / 1000                              in
+            printf "it_time: %d ns, time remaining %d.%d secs@\n%!" it_time secs msecs;
           rtm := rtn
         end;
-      if not (k i)       then raise (TestFailure i)
+      if not (k i)  then raise (TestFailure i)
     done;
     true
   with
   | TestFailure i -> eprintf "failed at number %d\n%!" i;
     false
+
+g
